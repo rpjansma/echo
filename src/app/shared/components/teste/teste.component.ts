05721544d9bddf99ddd66a6e840fc849ea7fdf64
Douglas <douglasandradeoliv10@gmail.com>
2021-10-27 22:04:48 -0300

    atualização da home + imagens

---
+++ src/app/shared/components/teste/teste.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test-componet',
+  templateUrl: './teste.component.html',
+  styleUrls: ['./teste.component.scss']
+})
+export class TestComponetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
